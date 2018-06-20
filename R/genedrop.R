8985daf32f3fc6d86dfe18f55941cf670cbde126
magnusdv <magnusdv@medisin.uio.no>
2018-06-20 15:37:37 +0200

    Introduce S3 class 'chromosomSim' and associated print method using glue.

--- R/genedrop.R
+++ R/genedrop.R
@@ -54,11 +54,37 @@ genedrop = function(x, map, condition=NULL, model="chi", skip.recomb=NULL) { # x
     attr(h, "dis.locus") = dis.locus
     attr(h, "dis.allele") = dis.al
   }
-  attr(h, "length_Mb") = attr(map, "length_Mb")
   attr(h, "chromosome") = chrom
+  attr(h, "length_Mb") = attr(map, "length_Mb")
+  attr(h, 'pedigree') = x
+  attr(h, "condition") = condition
+  attr(h, "model") = model
+  attr(h, "skipped") = skip.recomb
+  
+  class(h) = "chromosomeSim"
   h
 }
 
+print.chromosomeSim = function(x, ...) {
+  chrom = attr(x, 'chromosome')
+  len = attr(x, 'length_Mb')
+  model = attr(x, 'model')
+  pedsize = attr(x, 'pedigree')$NIND
+  skipped = attr(x, 'skipped')
+  skip_str = if(!length(skipped)) "None" else paste(skipped, collapse=",")
+  cond_sap = attr(x, 'condition')
+  cond = if(is.null(cond_sap)) "No" else cond_sap
+  
+  print(glue::glue("
+  Simulation of a single chromosome throughout a pedigree.
+  Chromosome name: {chrom}
+  Chromosome length: {len} Mb
+  Recombination model: {model}
+  Pedigree members: {pedsize}
+  Skipped recombination in: {skip_str}
+  Conditional: {cond}
+  "))
+}
 
 distribute.founder.alleles = function(x, chrom="AUTOSOMAL") {
   h = vector("list", x$NIND)
