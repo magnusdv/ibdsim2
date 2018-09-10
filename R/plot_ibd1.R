db9c3722e1f71e822c5155f30f278cb5676f51c8
magnusdv <magnusdv@medisin.uio.no>
2018-09-10 11:46:39 +0200

    Modify `plot_ibd1()` appearance:
    * Different kappa1 curves now have different line types
    * Kappa1 values now appear in a legend instead of besides its curve
    * Title and axis labels can now be specified by the user
    
    Closes #4

--- R/plot_ibd1.R
+++ R/plot_ibd1.R
@@ -20,7 +20,7 @@
 #' @param alpha A transparency parameter for the scatter points.
 #' @param ellipses A logical: Should confidence ellipses be added to the plot?
 #' @param legend_inside A logical indicating wether the legend should be placed inside (default) or outside the plot window.
-#'
+#' @param title,xlab,ylab Title and axis labels. Set to `NULL` to remove.
 #' @return A ggplot2 plot object.
 #'
 #' @examples
@@ -44,7 +44,9 @@
 #' @import ggplot2
 #' @importFrom ribd ibd_kinship
 #' @export
-plot_ibd1 = function(..., labels, alpha=1, ellipses=TRUE, legend_inside=TRUE) {
+plot_ibd1 = function(..., labels, alpha = 1, ellipses = TRUE, legend_inside = FALSE, 
+                     title = "Distribution of IBD segments", xlab = "Segment count", 
+                     ylab = "Average segment length (cM)") {
   sims = list(...)
   if(missing(labels)) labels = seq_along(sims)
   stopifnot(length(labels) == length(sims))
@@ -52,13 +54,13 @@ plot_ibd1 = function(..., labels, alpha=1, ellipses=TRUE, legend_inside=TRUE) {
   plot_data = lapply(seq_along(sims), function(i) {
     s = sims[[i]]
     ped = attr(s, 'pedigree')
-    ids = pedtools::leaves(ped)
+    ids = leaves(ped)
     real = realised_kappa(s, id.pair=ids)
     if(any(real$Nsegments['Nseg2', ] > 0)) 
        message("Warning: Simulation list ", i, " includes IBD=2 segments. Expected kappa1 line will be wrong!")
     count = real$Nsegments["Nseg1", ]
     averlen = ifelse(count == 0, 0, real$kappa.realised['ibd1', ] * real$genomeLength / count)
-    kinship_coeff = ribd::ibd_kinship(ped)[ids[1], ids[2]]
+    kinship_coeff = ibd_kinship(ped)[ids[1], ids[2]]
   
     data.frame(count = count,
                averlen = averlen, 
@@ -74,42 +76,38 @@ plot_ibd1 = function(..., labels, alpha=1, ellipses=TRUE, legend_inside=TRUE) {
   max.y = max(plot_data$averlen)
   
   g = ggplot(data=plot_data, aes_string(x="count", y="averlen", col="relation")) + 
-    geom_jitter(width=0.25, alpha=alpha) +
-    theme_bw(base_size=15) + 
+    geom_jitter(width = 0.25, alpha = alpha) +
+    theme_bw(base_size = 15) + 
     scale_color_manual(values = ggplotColors(nlevels(plot_data$relation))) +
-    labs(title = "Distribution of IBD segments", 
-         x = "Segment count", 
-         y = "Average length (cM)", 
-         col = "Relationship")
+    labs(title = title, x = xlab, y = ylab, col = "Relationship")
     
   if(ellipses) 
-    g = g + stat_ellipse(aes_string(x="count"), size=1.3)
-  
-  if(legend_inside) 
-    g = g + theme(legend.position = c(.95, .95), legend.justification = c("right", "top")) 
-  
+    g = g + stat_ellipse(aes_string(x = "count"), size=1.3)
   
   # Theoretical ibd1 curves
   ibd1_vals = sort(unique(plot_data$ibd1_theory))
   genomeLen = plot_data$genomeLength[1]
   
-  pointlist = lapply(ibd1_vals, function(ibd1) {
+  curveData = do.call(rbind, lapply(ibd1_vals, function(ibd1) {
     xmin = genomeLen * ibd1 / max.y
-    xvec = seq(xmin, max.x, length=100)
-    data.frame(IBD1=ibd1, x=xvec, y=genomeLen*ibd1/xvec)
-  })
-  
-  contour.data = do.call(rbind, pointlist)
-  contour.data$IBD1 = as.factor(contour.data$IBD1)
-  
-  # kappa curve labels
-  kappa_labels = paste("kappa[1] == ", round(ibd1_vals,3))
-  ymin = sapply(pointlist, function(df) df$y[nrow(df)])
-  vj = rep_len(c(1.5, -0.5), length(ymin)) # vjust: alternating below/above
+    xvec = seq(xmin, max.x, length=10)
+    data.frame(x = xvec, y = genomeLen*ibd1/xvec, kappa1 = as.character(round(ibd1, 3)))
+  }))
   
   g = g + 
-    geom_line(data=contour.data, aes_string("x", "y", group="IBD1"), lty=2, lwd=1, inherit.aes = F) +
-    annotate("text", max.x, ymin, label=kappa_labels, hjust="right", vjust=vj, parse=T, size=5)
+    geom_line(data = curveData, aes_string("x", "y", linetype = "kappa1"), lwd = 1, inherit.aes = F) + 
+    scale_linetype_manual(values = 1 + seq_along(ibd1_vals), # avoid solid line
+                          name = expression(Theoretical~kappa[1]))
+  
+  # Fix legends
+  g = g + 
+    theme(legend.key.width = unit(0.9, "cm")) + 
+    guides(color = guide_legend(order = 1), 
+           linetype = guide_legend(order = 2, reverse = T))
+  
+  if(legend_inside) 
+    g = g + theme(legend.position = c(.95, .95), 
+                  legend.justification = c("right", "top"))
   
   g
 }
