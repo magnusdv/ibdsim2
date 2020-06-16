bdad983a2d2085b694a4cc6627e93e614f2ecc6c
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2020-06-16 09:32:36 +0200

    Remove unused code

--- R/utils.R
+++ R/utils.R
@@ -67,21 +67,6 @@ pos2allele = function(haplo, posvec) { # haplo = matrix with 2 columns (breaks -
   haplo[indices, 2]
 }
 
-# Unused at the moment
-#locus2marker = function(x, h, locus) {
-#  marker = t.default(sapply(h, .getAlleles, posvec = locus))
-#  paramlink::setMarkers(x, marker)
-#}
-
-.printSAP = function(sap) {
-  if (!is.null(two <- sap[["2"]])) cat("  Two copies:", paste(two, collapse = ", "), "\n")
-  if (!is.null(atl1 <- sap[["atleast1"]])) cat("  At least one copy:", paste(atl1, collapse = ", "), "\n")
-  if (!is.null(one <- sap[["1"]])) cat("  One copy:", paste(one, collapse = ", "), "\n")
-  if (!is.null(atm1 <- sap[["atmost1"]])) cat("  At most one copy:", paste(atm1, collapse = ", "), "\n")
-  if (!is.null(zero <- sap[["0"]])) cat("  Zero copies:", paste(zero, collapse = ", "), "\n")
-  cat("\n")
-}
-
 .sortDouble = function(x) {
   x[order(x, method = "shell")]
 }
