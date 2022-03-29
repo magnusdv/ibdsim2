490f2b43f4b73ee0f0530f67c14596b9c97b427c
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2022-03-29 11:38:54 +0200

    Rewrite `profileSimIBD()`, with massive speed improvement

--- R/profileSimIBD.R
+++ R/profileSimIBD.R
@@ -4,17 +4,18 @@
 #' specific underlying IBD pattern.
 #'
 #' It should be noted that the only *random* part of this function is the
-#' selection of founder alleles for each marker. Given those, all other
-#' genotypes in the pedigree are determined by the underlying IBD pattern.
+#' sampling of founder alleles for each marker. Given those, all other genotypes
+#' in the pedigree are determined by the underlying IBD pattern.
 #'
 #' @param x A `ped` object.
 #' @param ibdpattern A `genomeSim()` object, typically created by [ibdsim()].
 #'   (See Examples).
-#' @param ids A vector of ID labels. If NULL, all members of `x` are included.
+#' @param ids A vector of ID labels. If NULL, extracted from `ibdpattern`.
 #' @param markers A vector with names or indices of markers attached to `x`.
 #' @param seed An integer seed for the random number generator.
 #'
-#' @return An object similar to `x`. but with simulated genotypes.
+#' @return A copy of `x` where the genotypes of the selected markers have been
+#'   replaced with simulated ones.
 #'
 #' @seealso [ibdsim()]
 #'
@@ -39,7 +40,7 @@
 #'
 #' @export
 profileSimIBD = function(x, ibdpattern, ids = NULL, markers = NULL, seed = NULL) {
-
+  
   # Set seed if given
   if(!is.null(seed))
     set.seed(seed)
@@ -49,70 +50,71 @@ profileSimIBD = function(x, ibdpattern, ids = NULL, markers = NULL, seed = NULL)
   
   if(!is.null(markers))
     x = selectMarkers(x, markers)
-
+  
   a = ibdpattern
   if(is.null(ids))
     ids = extractIds(a)
   else
     a = alleleFlow(a, ids, addState = FALSE)
-  
+
   if(!all(ids %in% labels(x)))
     stop2("ID label in `ibdpattern` not found in `x`: ", setdiff(ids, labels(x)))
   
-  achr = a[, 'chrom']
+  idsInt = internalID(x, ids)
   
+  # Split a on chrom (NB: split(a, a[,'chrom']) doesn't work directly)
+  aChr = lapply(split(1:nrow(a), a[, "chrom"]), function(rws) a[rws, , drop = FALSE])
+
   nMark = nMarkers(x)
   mchr  = chrom(x, 1:nMark)
   mpos  = posMb(x, 1:nMark)
   
   if(any(is.na(mpos) | is.na(mchr)))
     stop2("All markers must have defined chromosome and position attributes")
-
-  if(!all(mchr %in% achr))
+  
+  if(!all(mchr %in% names(aChr)))
     stop2("Chromosome missing from `ibdpattern`: ", setdiff(mchr, achr))
-    
-  # Pick rows (indices) in `a` corresponding to marker positions
-  arows = vapply(seq_len(nMark), function(i) {
-    chrrows = which(achr == mchr[i])
-    if(length(chrrows) == 1)
-      chrrows
-    else
-      chrrows[findInterval(mpos[i], a[chrrows, 'start'], all.inside = TRUE)]
-  }, FUN.VALUE = 1L)
   
   # Allele columns
-  acols = 4 + seq_len(2 * length(ids))
+  matcols = 4 + seq_along(ids)*2L
+  patcols = matcols - 1L
   
   # Number of founder alleles (i.e,. "different colours")
-  nA = 2 * length(founders(x))
+  f2 = 2 * length(founders(x))
   
-  # Create empty allele matrix
-  alsMat.transp = matrix("0", nrow = 2*nMark, ncol = length(ids))
+  # Marker matrix template
+  tmpMat = matrix(0L, nrow = pedsize(x), ncol = 2)
   
   # Fill in allele matrix one marker at a time
   for(i in seq_len(nMark)) {
-    # IBD pattern for this marker
-    ibdpatt = a[arows[i], acols]
-
-    # Sample founder allele labels
-    frqvec = afreq(x, i)
-    als = names(frqvec)
-    founderAlleles = sample(als, size = nA, replace = TRUE, prob = frqvec)
-
-    # Distribute alleles according to IBD pattern
-    allAlleles = founderAlleles[ibdpatt]
     
-    # Insert in transposed matrix
-    alsMat.transp[c(2*i-1, 2*i), ] = allAlleles
+    # IBD pattern for this marker
+    achr = aChr[[mchr[i]]]
+    rw = if(nrow(achr) > 1) findInterval(mpos[i], achr[, 'start'], all.inside = TRUE) else 1L
+    ibdpat = achr[rw, patcols]
+    ibdmat = achr[rw, matcols]
+    
+    # Marker allele frequencies
+    m = x$MARKERS[[i]]
+    frq = attr(m, "afreq")
+    
+    # Sample founder alleles
+    founderAlleles = sample.int(length(frq), size = f2, replace = TRUE, prob = frq)
+    
+    # Distribute alleles according to IBD pattern
+    amat = tmpMat
+    amat[idsInt, 1] = founderAlleles[ibdpat]
+    amat[idsInt, 2] = founderAlleles[ibdmat]
+    
+    # Sort genotypes
+    swap = amat[,1] > amat[,2]
+    if(any(swap))
+      amat[swap, 1:2] = amat[swap, 2:1]
+    
+    # Insert in marker object
+    x$MARKERS[[i]][] = amat
   }
   
-  # Transpose back
-  alsMat = t.default(alsMat.transp)
-  
-  # Attach and return
-  y = setAlleles(x, ids = ids, alleles = alsMat)
-  y = sortGenotypes(y)
-  
-  y
+  x
 }
 
