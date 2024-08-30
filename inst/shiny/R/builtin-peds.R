8bc6d932f215b9538c3f20d4f56cb295c2ed68a9
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2024-08-30 14:14:20 +0200

    Fix wrong-labelled incest pedigrees
    * Add a few more builtin pedigrees
    * Tweak pedigree names

--- inst/shiny/R/builtin-peds.R
+++ inst/shiny/R/builtin-peds.R
@@ -10,26 +10,29 @@ BUILTIN_PEDS = list(
   "Avuncular, paternal" = nuclearPed(2) |> addSon(4, verbose = FALSE),
   "Grandparent (female line)" = linearPed(2, sex = 2),
   "Grandparent (male line)" = linearPed(2),
-  "Great grandparent (female line)" = linearPed(3, sex = 2),
-  "Great grandparent (male line)" = linearPed(3),
-  "1st cousins" = cousinPed(1),
-  "1st cousins + child" = cousinPed(1, child = TRUE),
-  "2nd cousins" = cousinPed(2),
-  "2nd cousins + child" = cousinPed(2, child = TRUE),
-  "Half 1st cousins" = halfCousinPed(1),
-  "Half 1st cousins + child" = halfCousinPed(1, child = TRUE),
-  "Half 2nd cousins" = halfCousinPed(2),
-  "Half 2nd cousins + child" = halfCousinPed(2, child = TRUE),
+  "Great grandp (female line)" = linearPed(3, sex = 2),
+  "Great grandp (male line)" = linearPed(3),
+  "1st cousins" = cousinPed(1, symmetric = TRUE),
+  "1st cousins + child" = cousinPed(1, symmetric = TRUE, child = TRUE),
+  "2nd cousins" = cousinPed(2, symmetric = TRUE),
+  "2nd cousins + child" = cousinPed(2, symmetric = TRUE, child = TRUE),
+  "Half 1st cousins" = halfCousinPed(1, symmetric = TRUE),
+  "Half 1st cousins + child" = halfCousinPed(1, symmetric = TRUE, child = TRUE),
+  "Half 2nd cousins" = halfCousinPed(2, symmetric = TRUE),
+  "Half 2nd cousins + child" = halfCousinPed(2, symmetric = TRUE, child = TRUE),
   "3/4-siblings" = nuclearPed(2) |> addSon(c(3,5), verbose = FALSE) |> addSon(4:5),
   "3/4-siblings + child" = nuclearPed(2) |> addSon(c(3,5), verbose = FALSE) |> addDaughter(4:5) |> addSon(6:7),
-  "Double 1st cousins" = doubleFirstCousins(),
-  "Double 1st cousins + child" = doubleCousins(1, 1, child = TRUE),
+  "Dbl 1st cousins" = doubleFirstCousins(),
+  "Dbl 1st cousins + child" = doubleCousins(1, 1, child = TRUE),
   "Quad half 1st cousins" = quadHalfFirstCousins(),
   "Father-daughter incest" = nuclearPed(1, sex = 2) |> addSon(c(1,3)),
   "Mother-son incest" = nuclearPed(1) |> addSon(2:3),
-  "Full-sib incest" = fullSibMating(1),
-  "Half-sib incest" = halfSibStack(2)
+  "Full-sib incest" = nuclearPed(2, sex = 1:2) |> addSon(3:4),
+  "Half-sib incest" = halfSibPed(sex2 = 2) |> addSon(4:5),
+  "Grandfather incest" = linearPed(2, sex = 1:2) |> addSon(c(1,5)),
+  "Grandmother incest" = linearPed(2, sex = 2:1) |> addSon(c(2,5))
 )
+
 # Default individuals to be indicated when a pedigree is loaded
 DEFAULT_IDS = lapply(BUILTIN_PEDS, leaves)
 
